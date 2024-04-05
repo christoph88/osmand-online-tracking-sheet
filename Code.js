709b8c3c7f9d6ad469d6d713b5f3b9bf3b10a3e6
christoph88 <christoph88@users.noreply.github.com>
2024-04-05 09:43:11 +0200

    add proper page name

--- Code.js
+++ Code.js
@@ -1,17 +1,23 @@
 // Function to fetch lat and lon values and return the OpenStreetMap URL
 function getLatLonAndRedirect(spreadsheet) {
+  var latRange = spreadsheet.getRangeByName("lat");
+  var lonRange = spreadsheet.getRangeByName("lon");
 
-
-  var latRange = spreadsheet.getRangeByName('lat');
-  var lonRange = spreadsheet.getRangeByName('lon');
-  
   var lat = latRange.getValue();
   var lon = lonRange.getValue();
-  
-  var openMapsUrl = "https://www.openstreetmap.org/?mlat=" + lat + "&mlon=" + lon + "#map=10/" + lat + "/" + lon;
+
+  var openMapsUrl =
+    "https://www.openstreetmap.org/?mlat=" +
+    lat +
+    "&mlon=" +
+    lon +
+    "#map=10/" +
+    lat +
+    "/" +
+    lon;
 
   console.log(openMapsUrl);
-  
+
   return openMapsUrl;
 }
 
@@ -20,29 +26,48 @@ function doGet(e) {
   var params = e.parameter;
   console.log(params);
   var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-  var sheet = spreadsheet.getSheetByName('data');
+  var sheet = spreadsheet.getSheetByName("data");
 
-  if (params.lat && params.lon && params.timestamp && params.hdop && params.altitude && params.speed) {
+  if (
+    params.lat &&
+    params.lon &&
+    params.timestamp &&
+    params.hdop &&
+    params.altitude &&
+    params.speed
+  ) {
     // Clear the sheet before appending the new row
     sheet.clear();
 
     // Append the new row
-    sheet.appendRow(['lat', 'lon', 'timestamp', 'hdop', 'altitude', 'speed']);
-    sheet.appendRow([params.lat, params.lon, params.timestamp, params.hdop, params.altitude, params.speed]);
+    sheet.appendRow(["lat", "lon", "timestamp", "hdop", "altitude", "speed"]);
+    sheet.appendRow([
+      params.lat,
+      params.lon,
+      params.timestamp,
+      params.hdop,
+      params.altitude,
+      params.speed,
+    ]);
 
     // Return a simple message to acknowledge the reception
     return ContentService.createTextOutput("Parameters logged successfully.");
   }
-  
+
   // Generate the OpenStreetMap URL
   var redirectUrl = getLatLonAndRedirect(spreadsheet);
-  
-  // Create an HTML output that displays the URL as a clickable link
+
+  // Create an HTML output that displays the URL as a clickable link with a custom page title
   var htmlOutput = HtmlService.createHtmlOutput(
-    "<a href='" + 
-    redirectUrl + 
-    "' target='_blank'>Bekijk laatste locatie</a>"
+    "<html>" +
+      "<head><title>View my location</title></head>" +
+      "<body>" +
+      "Click the link to view the location on OpenStreetMap: <a href='" +
+      redirectUrl +
+      "' target='_blank'>OpenStreetMap Link</a>" +
+      "</body>" +
+      "</html>"
   );
-  
+
   return htmlOutput;
 }
