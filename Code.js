a18b47f726f2194980f9c3ad794e0def797b3b67
christoph88 <christoph88@users.noreply.github.com>
2024-04-05 13:40:32 +0200

    update code

--- Code.js
+++ Code.js
@@ -1,19 +1,51 @@
+function isWithinRadius(lat1, lon1, lat2, lon2, radius) {
+  const R = 6371; // Earth’s radius in kilometers
+  const dLat = toRadians(lat2 - lat1);
+  const dLon = toRadians(lon2 - lon1);
+  const rLat1 = toRadians(lat1);
+  const rLat2 = toRadians(lat2);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.cos(rLat1) * Math.cos(rLat2) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in kilometers
+
+  return distance <= radius;
+}
+
+function toRadians(degrees) {
+  return degrees * (Math.PI / 180);
+}
+
 // Function to fetch lat and lon values and return the OpenStreetMap URL
 function getLocation(spreadsheet) {
   var lat = spreadsheet.getRangeByName("lat").getValue();
   var lon = spreadsheet.getRangeByName("lon").getValue();
 
+  var latHome = 51.12509;
+  var lonHome = 5.3676;
+  var kmRadius = 0.5;
+  const withinSafezone = isWithinRadius(latHome, lonHome, lat, lon, kmRadius);
+  var safeLat = withinSafezone ? latHome : lat;
+  var safeLon = withinSafezone ? lonHome : lon;
+
   var openMapsUrl =
     "https://www.openstreetmap.org/?mlat=" +
-    lat +
+    safeLat +
     "&mlon=" +
-    lon +
+    safeLon +
     "#map=10/" +
-    lat +
+    safeLat +
     "/" +
-    lon;
+    safeLon;
 
-  return { lat: lat, lon: lon, url: openMapsUrl };
+  return {
+    lat: safeLat,
+    lon: safeLon,
+    url: openMapsUrl,
+    withinSafezone: withinSafezone,
+  };
 }
 
 function doGet(e) {
@@ -60,6 +92,7 @@ function createTemplate(spreadsheet) {
   var template = HtmlService.createTemplateFromFile("MapView");
   // Pass variables to the HTML template
   template.mapUrl = location.url;
+  template.withinSafezone = location.withinSafezone;
   template.lat = location.lat;
   template.lon = location.lon;
   console.log(template);
