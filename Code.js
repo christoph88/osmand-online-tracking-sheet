9123438b55d1c0c70b063b0a7c7d9a3d1d5679b1
christoph88 <christoph88@users.noreply.github.com>
2024-04-05 09:22:07 +0200

    initial commit

---
+++ Code.js
@@ -0,0 +1,48 @@
+// Function to fetch lat and lon values and return the OpenStreetMap URL
+function getLatLonAndRedirect(spreadsheet) {
+
+
+  var latRange = spreadsheet.getRangeByName('lat');
+  var lonRange = spreadsheet.getRangeByName('lon');
+  
+  var lat = latRange.getValue();
+  var lon = lonRange.getValue();
+  
+  var openMapsUrl = "https://www.openstreetmap.org/?mlat=" + lat + "&mlon=" + lon + "#map=10/" + lat + "/" + lon;
+
+  console.log(openMapsUrl);
+  
+  return openMapsUrl;
+}
+
+function doGet(e) {
+  // Log the parameters received in the URL
+  var params = e.parameter;
+  console.log(params);
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = spreadsheet.getSheetByName('data');
+
+  if (params.lat && params.lon && params.timestamp && params.hdop && params.altitude && params.speed) {
+    // Clear the sheet before appending the new row
+    sheet.clear();
+
+    // Append the new row
+    sheet.appendRow(['lat', 'lon', 'timestamp', 'hdop', 'altitude', 'speed']);
+    sheet.appendRow([params.lat, params.lon, params.timestamp, params.hdop, params.altitude, params.speed]);
+
+    // Return a simple message to acknowledge the reception
+    return ContentService.createTextOutput("Parameters logged successfully.");
+  }
+  
+  // Generate the OpenStreetMap URL
+  var redirectUrl = getLatLonAndRedirect(spreadsheet);
+  
+  // Create an HTML output that displays the URL as a clickable link
+  var htmlOutput = HtmlService.createHtmlOutput(
+    "<a href='" + 
+    redirectUrl + 
+    "' target='_blank'>Bekijk laatste locatie</a>"
+  );
+  
+  return htmlOutput;
+}
